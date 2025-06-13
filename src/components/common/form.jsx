ee5714fc4f9dc82d91e31f537fd43f6689fd75d6
HamadIslamian <hamad.yousafzai34@gmail.com>
2025-06-13 17:13:48 +0500

    yes

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,114 +1,128 @@
-import { Label } from '../ui/label.jsx'
-import { Input } from '../ui/input.jsx';
-import React from 'react';
-import { Select, SelectValue, SelectContent, SelectTrigger } from '../ui/select.jsx';
-import { Textarea } from '../ui/textarea.jsx';
-import { Button } from '../ui/button.jsx';
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
 
-const types ={
-  INPUT:"input",
-  SELECT:"select",
-}
-
-const CommonForm = ({formControls,formData,setFormData,onSubmit,buttonText}) => {
-  function renderInputsByComponentType(getControlItem){
+function CommonForm({
+  formControls,
+  formData,
+  setFormData,
+  onSubmit,
+  buttonText,
+  // isBtnDisabled,
+}) {
+  function renderInputsByComponentType(getControlItem) {
     let element = null;
-    const value=formData(getControlItem.name) || ''
+    const value = formData[getControlItem.name] || "";
 
+    switch (getControlItem.componentType) {
+      case "input":
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+            value={value}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: event.target.value,
+              })
+            }
+          />
+        );
 
-    switch(getControlItem.componentType) {
-      case types.INPUT:
-        element=(<Input
-        name={getControlItem.name}
-        placeholder={getControlItem.placeholder}
-        id={getControlItem.name}
-        type={getControlItem.type}
-        value={value}
-        onChange={event=>setFormData(
-          {
-            ...formData,
-            [getControlItem.name] : event.target.value
-          }
-        )}
-        />
+        break;
+      case "select":
+        element = (
+          <Select
+            onValueChange={(value) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: value,
+              })
+            }
+            value={value}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder={getControlItem.label} />
+            </SelectTrigger>
+            <SelectContent>
+              {getControlItem.options && getControlItem.options.length > 0
+                ? getControlItem.options.map((optionItem) => (
+                    <SelectItem key={optionItem.id} value={optionItem.id}>
+                      {optionItem.label}
+                    </SelectItem>
+                  ))
+                : null}
+            </SelectContent>
+          </Select>
+        );
+
+        break;
+      case "textarea":
+        element = (
+          <Textarea
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.id}
+            value={value}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: event.target.value,
+              })
+            }
+          />
+        );
+
+        break;
+
+      default:
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+            value={value}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: event.target.value,
+              })
+            }
+          />
         );
         break;
-        case "select":
-          element=(<Select onValueChange={(value)=>setFormData({
-            ...formData,
-            [getControlItem.name] : value,
+    }
 
-          })}   value={value}>
-            <SelectTrigger className='w-full'>
-              <SelectValue placeholder={getControlItem.placeholder}/>
-          </SelectTrigger>
-          <SelectContent>
-            {
-              getControlItem.options && 
-              getControlItem.options.length > 0 ?
-              getControlItem.options.map((optionItem =><SelectItem key={optionItem.id} value={optionItem.id}>{optionItem.label}</SelectItem>))
-              : null
-
-            }
-          </SelectContent>
-          </Select>
-          );
-          break;
-
-          case "textarea":
-            element=(
-              <Textarea value={value} name={getControlItem.name} placeholder={getControlItem.placeholder} id={getControlItem.id} onChange={event=>setFormData(
-                {
-                  ...formData,
-                  [getControlItem.name] : event.target.value
-                }
-              )}>
-
-              </Textarea>
-            );
-            break;
-          default:
-            case "input":
-              element=(<Input
-              name={getControlItem.name}
-              placeholder={getControlItem.placeholder}
-              id={getControlItem.name}
-              type={getControlItem.type}
-              value={value} 
-              onChange={event=>setFormData(
-                {
-                  ...formData,
-                  [getControlItem.name] : event.target.value
-                }
-              )}
-              />
-              );
-              break;
-    }   
-  
+    return element;
   }
+
   return (
     <form onSubmit={onSubmit}>
-      <div className='flex flex-col gap-3'>
-        {
-          (formControls || []).map(controlItem =>
-         
-      
-          <div key={controlItem.name} className='grid w-full gap-1.5'>
+      <div className="flex flex-col gap-3">
+        {formControls.map((controlItem) => (
+          <div className="grid w-full gap-1.5" key={controlItem.name}>
             <Label className="mb-1">{controlItem.label}</Label>
-            {
-              renderInputsByComponentType(controlItem) 
-            }
-
+            {renderInputsByComponentType(controlItem)}
           </div>
-          )
-        }
+        ))}
       </div>
-      <Button type="Submit" className="mt-2 w-full">{buttonText || "Submit"}</Button>
-
-
+      <Button disabled={isBtnDisabled} type="submit" className="mt-2 w-full">
+        {buttonText || "Submit"}
+      </Button>
     </form>
-  )
+  );
 }
 
-export default CommonForm 
+export default CommonForm;
--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,6 +1,12 @@
-import CommonForm from '@/components/common/form'
-import React, { useState } from 'react'
-import { Link } from 'react-router'
+import CommonForm from "@/components/common/form";
+// import { useToast } from "@/components/ui/use-toast";
+import { registerFormControls } from "@/config";
+// import { registerUser } from "@/store/auth-slice";
+import { useState } from "react";
+// import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+
 
 const initialState ={
   userName : "",
@@ -8,34 +14,35 @@ const initialState ={
   password:"",
 }
 
-const AuthRegister = ({formControls}) => {
+function AuthRegister() {
   const [formData,setFormData]=useState(initialState)
   const onSubmit=()=>{
 
   }
   return (
-    <div className='mx-auto w-full max-w-md space-y-6'>
-      <div className='text-center'>
-        <h1 className='text-3xl font-bold tracking-tight text-foreground'>Create new account</h1>
-        <p className='mt-2'>
-          Already have an account
+    <div className="mx-auto w-full max-w-md space-y-6">
+    <div className="text-center">
+      <h1 className="text-3xl font-bold tracking-tight text-foreground">
+        Create new account
+      </h1>
+      <p className="mt-2">
+        Already have an account
         <Link
-        className='font-medium text-primary hover:underline'
-         to="/auth/login">Login
+          className="font-medium ml-2 text-primary hover:underline"
+          to="/auth/login"
+        >
+          Login
         </Link>
-        </p>
-
-      </div>
-      <CommonForm 
-      formControls={formControls}
+      </p>
+    </div>
+    <CommonForm
+      formControls={registerFormControls}
       buttonText={"Sign Up"}
       formData={formData}
       setFormData={setFormData}
       onSubmit={onSubmit}
-      />
-       
-
-    </div>
+    />
+  </div>
   )
 }
 
