85395d036addc74008175ea397ac17ac282e0572
HamadIslamian <hamad.yousafzai34@gmail.com>
2025-08-03 23:49:09 +0500

    yes

--- src/store/admin/products-slice/index.js
+++ src/store/admin/products-slice/index.js
@@ -2,84 +2,134 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-  isLoading: false,
-  productList: [],
+  isAuthenticated: false,
+  isLoading: true,
+  user: null,
 };
 
-export const addNewProduct = createAsyncThunk(
-  "/products/addnewproduct",
+export const registerUser = createAsyncThunk(
+  "/auth/register",
+
   async (formData) => {
-    const result = await axios.post(
-      "http://localhost:5000/api/admin/products/add",
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/register",
       formData,
       {
-        headers: {
-          "Content-Type": "application/json",
-        },
+        withCredentials: true,
       }
     );
 
-    return result?.data;
+    return response.data;
   }
 );
 
-export const fetchAllProducts = createAsyncThunk(
-  "/products/fetchAllProducts",
+export const loginUser = createAsyncThunk(
+  "/auth/login",
+
+  async (formData) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/login",
+      formData,
+      {
+        withCredentials: true,
+      }
+    );
+
+    return response.data;
+  }
+);
+
+export const logoutUser = createAsyncThunk(
+  "/auth/logout",
+
   async () => {
-    const result = await axios.get(
-      "http://localhost:5000/api/admin/products/get"
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/logout",
+      {},
+      {
+        withCredentials: true,
+      }
     );
 
-    return result?.data;
+    return response.data;
   }
 );
 
-export const editProduct = createAsyncThunk(
-  "/products/editProduct",
-  async ({ id, formData }) => {
-    const result = await axios.put(
-      `http://localhost:5000/api/admin/products/edit/${id}`,
-      formData,
+export const checkAuth = createAsyncThunk(
+  "/auth/checkauth",
+
+  async () => {
+    const response = await axios.get(
+      "http://localhost:5000/api/auth/check-auth",
       {
+        withCredentials: true,
         headers: {
-          "Content-Type": "application/json",
+          "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
         },
       }
     );
 
-    return result?.data;
+    return response.data;
   }
 );
 
-export const deleteProduct = createAsyncThunk(
-  "/products/deleteProduct",
-  async (id) => {
-    const result = await axios.delete(
-      `http://localhost:5000/api/admin/products/delete/${id}`
-    );
-
-    return result?.data;
-  }
-);
-
-const AdminProductsSlice = createSlice({
-  name: "adminProducts",
+const authSlice = createSlice({
+  name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    setUser: (state, action) => {},
+  },
   extraReducers: (builder) => {
     builder
-      .addCase(fetchAllProducts.pending, (state) => {
+      .addCase(registerUser.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(fetchAllProducts.fulfilled, (state, action) => {
+      .addCase(registerUser.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.productList = action.payload.data;
+        state.user = null;
+        state.isAuthenticated = false;
       })
-      .addCase(fetchAllProducts.rejected, (state, action) => {
+      .addCase(registerUser.rejected, (state, action) => {
         state.isLoading = false;
-        state.productList = [];
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        console.log(action);
+
+        state.isLoading = false;
+        state.user = action.payload.success ? action.payload.user : null;
+        state.isAuthenticated = action.payload.success;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(checkAuth.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(checkAuth.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload.success ? action.payload.user : null;
+        state.isAuthenticated = action.payload.success;
+      })
+      .addCase(checkAuth.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(logoutUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
       });
   },
 });
 
-export default AdminProductsSlice.reducer;
+export const { setUser } = authSlice.actions;
+export default authSlice.reducer;
